be72124e70cc8aca9a007be9f67837cfe632a0e3
hayden <haydenchuter@gmail.com>
2017-08-27 15:16:02 -0700

    Basic setup

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+ 
+int main() {
+    cout << "!!!Hello World From Pi!!!" << endl;
+    int x = 0;
+	x++;
+	cout << x << endl;
+	char userin[30];
+	cin >> userin;
+	cout << userin;
+    return 0;
+}
